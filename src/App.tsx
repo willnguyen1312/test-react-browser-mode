fd420f4ada693ac7666bff28c58a9e118b0ba8d6
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-10-26 11:01:40 -0600

    🧪

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <main>
+      <h1>Number: {number}</h1>
+      <button
+        disabled={loading}
+        onClick={async () => {
+          setLoading(true);
+          const randomID = Math.floor(Math.random() * 200) + 1;
+          const response = await fetch(
+            `https://jsonplaceholder.typicode.com/todos/${randomID}`
+          );
+          const data = await response.json();
+          setNumber(data.id);
+          setLoading(false);
+        }}
+      >
+        {loading ? "Loading..." : "Randomize number"}
+      </button>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/HelloWorld.test.tsx
@@ -0,0 +1,47 @@
+import { beforeEach, afterEach, expect, test, describe, vi } from "vitest";
+import { page, userEvent } from "vitest/browser";
+import { render } from "vitest-browser-react";
+import App from "./App.tsx";
+
+describe("App Component", () => {
+  const originalFetch = window.fetch;
+
+  beforeEach(() => {
+    // @ts-ignore
+    window.fetch = vi.fn(async (...arg) => {
+      // Wait for 250ms to simulate network delay
+      await new Promise((resolve) => setTimeout(resolve, 250));
+      return Promise.resolve({
+        json: () => Promise.resolve({ id: 999 }),
+      });
+    });
+  });
+
+  afterEach(() => {
+    window.fetch = originalFetch;
+  });
+
+  test("renders name", async () => {
+    vi.useFakeTimers();
+    // Arrange
+    await render(<App />);
+
+    // Assert
+    await expect
+      .element(page.getByRole("heading", { name: "number: 0" }))
+      .toBeVisible();
+
+    // Act
+    await userEvent.click(
+      page.getByRole("button", { name: "Randomize number" })
+    );
+    // Assert
+    await expect
+      .element(page.getByRole("button", { name: "Loading..." }))
+      .toBeDisabled();
+    await vi.runAllTimersAsync();
+    await expect
+      .element(page.getByRole("heading", { name: "number: 999" }))
+      .toBeVisible();
+  });
+});
